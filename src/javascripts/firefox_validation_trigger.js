1ed5c564a2d5ee6b60ce87c5b8b5a627296f9b2e
Adam Tal <atal@xogrp.com>
2016-08-12 10:02:15 -0500

    WIP: extract classes

--- src/javascripts/firefox_validation_trigger.js
+++ src/javascripts/firefox_validation_trigger.js
@@ -10,7 +10,17 @@ var TkFormFields = {
   validate: function(e) {
     setTimeout(function() {
       var emailInput = e.target;
-    }, 1000)
+      var emailValid = emailInput.checkValidity();
+      var tkFormFieldsDiv = document.getElementsByClassName('tk-form-fields')[0];
+      console.log('tkFormFieldsDiv: ', tkFormFieldsDiv);
+      if (emailValid) {
+        console.log('valid email');
+      } else {
+        console.log('INVALID email');
+        tkFormFieldsDiv.className += " input-valid-not-focused";
+        tkFormFieldsDiv.className += " input-invalid";
+      }
+    }, 10)
   }
 }
 
