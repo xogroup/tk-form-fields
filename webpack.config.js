7073c56ff86e1e30bb9f5fbc386bb2939205d6c4
pairing <no-reply@ideaful.co>
2016-10-18 18:11:55 -0400

    Introduce webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: `${__dirname}/public`,
+    filename: 'index.js',
+    library: ['XO', 'FormFields'],
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    modulesDirectories: ['node_modules']
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      PACKAGE_VERSION: JSON.stringify(require('./package.json').version)
+    })
+  ]
+};
+
